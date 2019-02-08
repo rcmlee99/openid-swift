039c2d3bf32077497d7a5b1497ae19ccee15211b
Roger Lee <rcmlee99@gmail.com>
2019-02-08 11:26:43 +1100

    Initial Commit

---
+++ Dev Portal/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Dev Portal
+//
+//  Created by Roger Lee on 8/2/19.
+//  Copyright © 2019 Roger Lee. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Dev Portal/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Dev Portal
+//
+//  Created by Roger Lee on 8/2/19.
+//  Copyright © 2019 Roger Lee. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
